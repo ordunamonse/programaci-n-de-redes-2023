3af1e844b7629defb2737ed2617d6a5801c2428c
ordunamonse <136828010+ordunamonse@users.noreply.github.com>
2023-09-11 11:50:27 -0600

    agrego nueva carpeta
    se agrego nueva carpeta

---
+++ calculadora.py
@@ -0,0 +1,41 @@
+'''
+Autor:Monserrat Orduña
+Fecha: 11 sep 2023
+Descripcion:calculadora que suma
+'''
+
+a= input ("ingresa el primer valor")
+b=input("ingresa el segundo valor")
+print (type (a), type (b))
+print ("Suma ", a + b)
+a= float (a)
+b= float (b)
+print (type (a), type (b))
+print ("Suma ", a + b)
+
+a= input ("ingresa el primer valor")
+b=input("ingresa el segundo valor")
+print (type (a), type (b))
+#print ("Resta ", a - b)
+a= int  (a)
+b= int (b)
+print (type (a), type (b))
+print ("Resta ", a - b) 
+
+a= input ("ingresa el primer valor")
+b=input("ingresa el segundo valor")
+print (type (a), type (b))
+#print ("Multiplicacion ", a * b) 
+a= float  (a)
+b= float  (b)
+print (type (a), type (b))
+print ("Multiplicacion ", a * b) 
+
+a= input ("ingresa el primer valor")
+b=input("ingresa el segundo valor")
+print (type (a), type (b))
+#print ("Division ", a / b)
+a= float  (a)
+b= float  (b)
+print (type (a), type (b))
+print ("Dividion ", a / b)  
--- saludar.py
+++ saludar.py
@@ -1,3 +1,8 @@
+'''
+Autor:monserrat orduña
+Fecha_11 septiembre 2023
+Descripcion: Iniciando con python
+'''
 nombre = input("ingresa tu nombre")
-print ('Hola', nombre)
+print ('Hola', nombre) #Imprime variables
 print("hola")
