f088dd284ad45cbcae52565ee48fa3c60e27310c
ordunamonse <136828010+ordunamonse@users.noreply.github.com>
2023-09-25 11:32:39 -0600

    unidad1_modulo1
    unidad1_modulo1

---
+++ modulo2/modulo3/laboratorio 3.1.1.4.py
@@ -0,0 +1,11 @@
+'''
+nombre:Monserat Orduña Basaldua
+Descripcion: uso de operadores relacionales
+Fecha:25/09/2023
+'''
+
+n = int(input("ingresa el numero"))
+print (type (n))
+
+n = int(input("ingresa el numero"))
+print (n >=100) #Imprime True o False
---
+++ modulo2/unidad1_modulo1/u1_prob1_usuariogithub.py
@@ -0,0 +1,20 @@
+'''
+Problema 1
+Una panadería vende barras de pan a $3.49 cada una. El pan que no es el día tiene un descuento del 60%.
+Escribe un programa que comience leyendo el número de barras vendidas que no son del día. 
+Después tu programa debe mostrar el precio habitual de una barra de pan, el descuento que se le hace 
+por no ser fresca y el coste final total.
+
+Nombre:Monserrat Orduña Basaldua
+Descripcion:problema 2
+Fecha:25/09/2023
+'''
+
+pan = int(input(3.49))
+
+print ("total de barras vendidas")
+
+else pan=$3,49 - 60%:
+     print(el total es )
+     
+     
