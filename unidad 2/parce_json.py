be86b9bc4f768f24e6c890c79e41056ccefea364
ordunamonse <136828010+ordunamonse@users.noreply.github.com>
2023-10-23 10:07:16 -0600

    unidad2
    unidad2

---
+++ unidad 2/parce_json.py
@@ -0,0 +1,25 @@
+'''
+Nombre:Monserat Orduña Basaldua 
+Fecha: 23 de octubre 2023
+Descripcion: Invocando  
+
+'''
+
+import urllib.parse
+import requests
+
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+orig = "Dolores Hidalgo"
+dest = "San Miguel de allende"
+key = "XxL9wmf7qb4t0P3QY2uB8UpVwoOTaCHV"
+
+url = main_api + urllib.parse.urlencode({"key": key, "from":orig, "to":dest}) 
+json_data = requests.get(url).json()
+print(json_data['route']['sessionId'])
+
+#Extrae la distancia  y el tiempo
+print(json_data['route']['distance'])
+print(json_data['route']['time'])
+
+#Extraer del primer elemento legs el campo formattedtime
+print(json_data['route']['formattedTime'])
